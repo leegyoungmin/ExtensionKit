605a743cf077335fdcb869b78a9cfd31227138e1
이경민 <cow970814@naver.com>
2024-03-22 16:48:43 +0900

    config : Demo App 생성

---
+++ ExtensionExampleApp/ExtensionExampleApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ExtensionExampleApp
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+import UIKit
+
+import ExtensionKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemBackground
+  }
+}
